d412a54b3975651193a32032dc633b25f30aa817
lerrybe <yeji.kim@dalpha.so>
2024-02-10 02:58:28 +0900

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["vite.svg"],
+      manifest: {
+        name: "pwa-sample",
+        short_name: "pwa-sample",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
